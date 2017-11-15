7c6f2bd60c66e555bebe4908dc866e67ab072a15
Evan Roman <eroman@klass3-oj-l.intra.itcollege.ee>
2017-11-15 13:28:07 +0200

    add tests

--- project/twittr/tests.py
+++ project/twittr/tests.py
@@ -1,3 +1,30 @@
-from django.test import TestCase
+from django.test import TestCase, RequestFactory
+from django.urls import reverse
 
-# Create your tests here.
+from twittr.models import Message
+from twittr.views import my_view
+
+
+class MyViewTestCase(TestCase):
+    def setUp(self):
+        self.factory = RequestFactory()
+        self.resource = reverse('my_view')
+
+    def test_get(self):
+        request = self.factory.get(self.resource)
+        response = my_view(request)
+        self.assertEqual(response.status_code, 200)
+
+    def test_post(self):
+        """
+        On a successful post request:
+        create new message in the database
+        return redirect back to same view
+        """
+        data = {'message': 'hi'}
+        request = self.factory.post(self.resource, data)
+        message_count_before = Message.objects.count()
+        response = my_view(request)
+        message_count_after = Message.objects.count()
+        self.assertEqual(message_count_before + 1, message_count_after)
+        self.assertEqual(response['location'], self.resource)
